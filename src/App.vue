e4b097520bb42d23180e85db6a4b6609fcd70a16
Vadim Cherepanov <xenos26@gmail.com>
2022-03-07 23:16:38 +0700

    double binding with function

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,91 @@
 <template>
-    <div>
-        
+  <div class="app">
+    <form>
+      <h4>Создание поста</h4>
+      <input
+        v-bind:value="title"
+        @input="inputTitle"
+        class="input"
+        type="text"
+        placeholder="Название"
+      />
+      <input
+        v-bind:value="body"
+        class="input"
+        type="text"
+        placeholder="Описание"
+      />
+      <button class="btn" @click="createPost">Создать</button>
+    </form>
+    <div class="post" v-for="post in posts">
+      <div><strong>Название:</strong> {{ post.title }}</div>
+      <div><strong>Описание:</strong> {{ post.body }}</div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                likes: -1,
-                dislikes: 0,
-            }
-        },
-        methods: {
-            addLike() {
-                this.likes += 1;
-            },
+export default {
+  data() {
+    return {
+      posts: [
+        { id: 1, title: "Javascript", body: "Описание поста" },
+        { id: 2, title: "Javascript 2", body: "Описание поста 2" },
+        { id: 3, title: "Javascript 3", body: "Описание поста 3" },
+        { id: 4, title: "Javascript 4", body: "Описание поста 4" },
+      ],
+      title: '',
+      body: '',
+    };
+  },
+  methods: {
+    createPost() {
 
-            addDislike() {
-                this.dislikes += 1;
-            }
-        },
+    },
+    inputTitle(event) {
+        this.title = event.target.value;
     }
+
+  }
+  
+};
 </script>
 
-<style lang="">
-    
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
+
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+
+.app {
+  padding: 20px;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.btn {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
 </style>

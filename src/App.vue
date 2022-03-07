34c28ece2d7e745ea7a11c32023f5aab585eee34
Vadim Cherepanov <xenos26@gmail.com>
2022-03-07 19:00:42 +0700

    add my first button and functional

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <button @click="addLike">Like</button>
+        <button @click="addDislike">Dislike</button>
+        <div>
+            Количество лайков: <strong>{{ likes }}</strong> 
+            Количество дизлайков: <strong>{{ dislikes }}</strong> 
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                likes: -1,
+                dislikes: 0,
+            }
+        },
+        methods: {
+            addLike() {
+                this.likes += 1;
+            },
+
+            addDislike() {
+                this.dislikes += 1;
+            }
+        },
+    }
+</script>
+
+<style lang="">
+    
+</style>
